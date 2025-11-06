616cb768482c44b60ea46cb29d1b8903b378d1f1
Harman Bains <bains.hmn@gmail.com>
2025-11-05 17:11:39 -0800

    reverse string answer

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,12 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let returnString = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    returnString = returnString.concat(str.charAt(i));
+  }
+  return returnString;
+}
 
 module.exports = reverse;
