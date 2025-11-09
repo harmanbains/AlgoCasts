e234efdd534a0d2ac570f5cc6961b159e4d73e8d
Harman Bains <bains.hmn@gmail.com>
2025-11-09 13:15:18 -0800

    maxchar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = new Map();
+  for (let char of str) {
+    if (charMap.has(char)) {
+      charMap.set(char, charMap.get(char) + 1);
+    } else {
+      charMap.set(char, 1);
+    }
+  }
+
+  let curMaxChar = '';
+  let curMaxCount = 0;
+  for (let char of charMap.entries()) {
+    if (char[1] > curMaxCount) {
+      curMaxChar = char[0];
+      curMaxCount = char[1];
+    }
+  }
+  return curMaxChar;
+}
 
 module.exports = maxChar;
